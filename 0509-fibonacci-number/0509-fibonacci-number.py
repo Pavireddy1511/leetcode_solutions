ebbc380f242a68a525b9dc1c0a41d7f93165c2bd
Pavireddy1511 <pavithrareddybatthula@gmail.com>
2026-08-18 17:45:34 +0530

    Time: 1020 ms (5.02%), Space: 19.2 MB (57.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,10 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
+        memo=[0]*(30+1)
         if(n==1):
              return 1
         if(n==0):
              return 0
-        return self.fib(n-1)+self.fib(n-2)
+        if(memo[n]!=0):
+            return memo[n]
+        memo[n]=self.fib(n-1)+self.fib(n-2)
+        return memo[n]
 
 
         
